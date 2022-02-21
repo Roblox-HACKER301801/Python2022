b7decdfb437cb551f517ef0ef1e7af1ab5b26aaf
Roblox-HACKER301801 <misshi93@kingshigh.school.nz>
2022-02-22 12:14:25 +1300

    Create 1_print_exercises.py

---
+++ 1_print_exercises.py
@@ -0,0 +1,22 @@
+#Print name
+print("Misha Shipin") 
+#Teachers name
+print("Luker smith")
+#Name of school.
+print("Kings high school")
+#Age
+print("15")
+#How many hours do I have in my fav game
+print("~1100")
+#What year will be in 25 years
+print(2022+25)
+#how much years willI be in 100 years?
+print(15+100)
+#devision question
+print(658/7)
+#how many years was the moon landing ago
+print(2022-1969)
+#Fav game
+print("SCP:SL")
+#fav class
+print("DTE or Maths probably cause the teachers are good")
