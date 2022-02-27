f14832f815c75d519ced2b131debef0f999714ea
Roblox-HACKER301801 <misshi93@kingshigh.school.nz>
2022-02-28 09:54:34 +1300

    Work done.

---
+++ 2_data_types.py
@@ -0,0 +1,53 @@
+#1
+print("\n酒干倘卖无 \n酒干倘卖无\n 酒干倘卖无\n 酒干倘卖无\n 酒干倘卖无\n 酒干倘卖无 \n 酒干倘卖无")
+#2
+print("\"The future of this country is the people\"-Mao Zedong")
+#3
+print("String=name of something, e.g \"Name\"")
+#4
+print("An interger is pretty much a number within code. \"E.g 1+1\"")
+#5
+print("A number with decimals in them") 
+#6
+print(543/3)
+#7
+print(543//3)
+#8
+print(543%3)
+#9-18
+print(1000%9)
+print(434//10)
+print(3%2)
+print(5%5)
+print(555/2)
+print(123/2)
+print(123//2)
+print(123%2)
+print(764%2)
+print(165//2)
+#19
+print(5591591.5192591+1525571985.1529591)
+#20
+print(44+15)
+#21
+print(1.41-240)
+#22
+print(210-125.55)
+#23
+print(153.4/198.1)
+#24
+print(24*215.14)
+#25
+print(2*15)
+#26
+print("misha"*10)
+#27 
+print("misha\n"*10)
+#28
+print("5+10="+ str(5+10))
+#29
+print("15/2="+ str(15/2)) 
+#30
+print("30/2*5="+ str(30/2*5))
+#31
+print("30/2*5="+ str(30/2*5))
---
+++ 4_modifyingvariables.py
@@ -0,0 +1,49 @@
+#1
+money=200
+hat=20
+top=30
+pants=15
+belt=60
+shoes=40
+#2
+print("You have $" +str(money)+ "in your bank account")
+#3
+print("You are buying an Aliexpress Hat") 
+print("Credit card accepted")
+#4
+money = money-hat
+#5
+print("You now have $" +str(money)+ "in your bank account")
+#6
+print(money)
+#7
+#TOP
+print("You have $" +str(money)+ "in your bank account")
+print("You are buying an Aliexpress top") 
+print("Credit card accepted")
+money = money-top
+print("You now have $" +str(money)+ "in your bank account")
+print(money)
+#PANTS
+print("You have $" +str(money)+ "in your bank account")
+print("You are buying an Aliexpress pants") 
+print("Credit card accepted")
+money = money-pants
+print("You now have $" +str(money)+ "in your bank account")
+print(money)
+#BELT
+print("You have $" +str(money)+ "in your bank account")
+print("You are buying an Aliexpress belt") 
+print("Credit card accepted")
+money = money-belt
+print("You now have $" +str(money)+ "in your bank account")
+print(money)
+#SHOES
+print("You have $" +str(money)+ "in your bank account")
+print("You are buying an Aliexpress shoes") 
+print("Credit card accepted")
+money = money-shoes
+print("You now have $" +str(money)+ "in your bank account")
+print(money)
+#ENDING
+print("Your balance is $" +str(money)+ "in your online bank account, thank you for shopping at Aliexpress")
