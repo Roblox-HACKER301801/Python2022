acfd2818da80e8949e88ba0dc304a39e41f965eb
Roblox-HACKER301801 <misshi93@kingshigh.school.nz>
2022-03-02 12:21:16 +1300

    Work

---
+++ 5_input.py
@@ -0,0 +1,40 @@
+#1
+"""
+random_key = input("Please press any key. Then followed by an enter.\n")
+print(f"that key was pressed, thank you") 
+#2
+name = input("What is your name?\n") 
+print(f"welcome {name}!") 
+#3
+age = input("please state your age\n")
+print(f"You are {age} years old.")
+#4
+n = input("Welcome to the CCP Help desk, please state your name.\n")
+#5
+a = input("nice to meet you citizen 9616 please could you state your age?\n") 
+#6
+fm = input("what is your fav movie?\n")
+#7
+fb = input("what is your fav book?\n") 
+#8
+rq = input("because you are part of the glorious provinces, we need to ask you a question do you like mao?") 
+#9 
+noun = input("please could you state a noun\n")
+#10 
+verb = input("pleaes could you also state a verb\n")
+#11
+print(f"So the information which you have given is this. \n your name is: {n} \n your age is: {a} \n your fav movie: {fm} \n your fav book is: {fb} \n hopefully you answered yes for the serious question (your response): {rq} \n The noun and verb which you had stated is: {noun} and {verb} \n If you want a cool username it would be: \n {fm}{fb}{a}.")
+#12 
+"""
+print("Welcome to the I-Apple™ Security Response system (Beta). \nPleae answer all questions and press \"Enter\" after you enter a response. You have been connected to \"Kelly Kasey\"")
+age = int(input("Please could you state your age\n"))
+#13
+print(f"You will be {age + 10} in 10 years.")
+#14
+print(f"You have been born in {2022 - age}.") 
+#15
+apples = int(input("How many apples (I-Apple™) Security products do you own?")) 
+#16
+friends = int(input("How many friends do you have added via I-Apple™ Security products?")) 
+#17
+print(f"You can successfully share {friends / apples} between you and your friends and should have ")
